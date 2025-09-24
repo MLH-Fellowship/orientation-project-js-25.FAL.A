a4e9410f0433ab8b95bc7a38ca910a15bc585fe0
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2025-09-23 21:06:56 -0400

    Format code with Prettier

--- src/Components/Skill/AddSkill.js
+++ src/Components/Skill/AddSkill.js
@@ -1,47 +1,47 @@
-import { useState, useEffect } from "react"
-import AddSkillForm from "./AddSkillForm"
-import DisplaySkill from "./DisplaySkill"
-import { getSkills } from "../../requests/skill"
+import { useState, useEffect } from "react";
+import AddSkillForm from "./AddSkillForm";
+import DisplaySkill from "./DisplaySkill";
+import { getSkills } from "../../requests/skill";
 
 export default function AddSkill() {
-    const [showForm, setShowForm] = useState(false)
-    const [skillData, setSkillData] = useState([])
+  const [showForm, setShowForm] = useState(false);
+  const [skillData, setSkillData] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await getSkills()
-                setSkillData(data)
-            } catch (error) {
-                console.error("Error fetching skill data:", error)
-            }
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getSkills();
+        setSkillData(data);
+      } catch (error) {
+        console.error("Error fetching skill data:", error);
+      }
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <section>
-            {skillData.length === 0 && <p>No skills added yet.</p>}
-            <div className="skill-wrapper">
-                {!showForm &&
-                    skillData.map((skill, index) => (
-                        <DisplaySkill
-                            key={index}
-                            name={skill.name}
-                            proficiency={skill.proficiency}
-                            logo={skill.logo}
-                        />
-                    ))}
-            </div>
+  return (
+    <section>
+      {skillData.length === 0 && <p>No skills added yet.</p>}
+      <div className="skill-wrapper">
+        {!showForm &&
+          skillData.map((skill, index) => (
+            <DisplaySkill
+              key={index}
+              name={skill.name}
+              proficiency={skill.proficiency}
+              logo={skill.logo}
+            />
+          ))}
+      </div>
 
-            {showForm ? (
-                <AddSkillForm setShowForm={setShowForm} />
-            ) : (
-                <>
-                    <p>You can add a new skill here</p>
-                    <button onClick={() => setShowForm(true)}>Add Skill</button>
-                </>
-            )}
-        </section>
-    )
+      {showForm ? (
+        <AddSkillForm setShowForm={setShowForm} />
+      ) : (
+        <>
+          <p>You can add a new skill here</p>
+          <button onClick={() => setShowForm(true)}>Add Skill</button>
+        </>
+      )}
+    </section>
+  );
 }
